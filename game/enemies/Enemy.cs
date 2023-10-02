d6901009e4909aa2d452649033649e5b34e117e7
Tyrannicodin16 <86689800+Tyrannicodin@users.noreply.github.com>
2023-10-02 20:58:19 +0100

    Add basic enemy movement

---
+++ game/enemies/Enemy.cs
@@ -0,0 +1,61 @@
+using Godot;
+using System;
+
+public class Enemy : Sprite
+{
+	private const int GRID_WIDTH = 10;
+	private const int GRID_HEIGHT = 10;
+	private const int TILEMAP_SIZE = 32;
+
+	private Vector2 GridPosition = new();
+	private readonly RandomNumberGenerator RNG = new();
+
+	public override void _Ready()
+	{
+		Offset = new(16, 16);
+		Texture = new AtlasTexture()
+		{
+			Atlas = ResourceLoader.Load("res://temp_sprites/enemies.png") as Texture,
+			Region = new Rect2(32, 0, new Vector2(32, 32))
+		};
+	}
+
+	public bool Move(Vector2 distance, bool use_tween = true)
+	{
+		var new_position = distance + GridPosition;
+		if (new_position.x >= GRID_WIDTH || new_position.y >= GRID_HEIGHT || new_position.x < 0 || new_position.y < 0)
+		{
+			return false;
+		}
+		GridPosition = new_position;
+		if (use_tween)
+		{
+			SceneTreeTween position_tween = CreateTween();
+			position_tween.TweenProperty(this, "position", GridPosition * TILEMAP_SIZE, 1);
+			position_tween.Play();
+		}
+		else
+		{
+			Position = GridPosition * TILEMAP_SIZE;
+		}
+		return true;
+	}
+
+	public void PlayerAction()
+	{ //TODO: Arguments here
+		Vector2 movement;
+		int direction;
+		do
+		{
+			direction = RNG.RandiRange(0, 3);
+			if (direction > 1)
+			{
+				movement = new(0, (direction - 2) * 2 - 1);
+			}
+			else
+			{
+				movement = new(direction * 2 - 1, 0);
+			}
+		} while (!Move(movement));
+	}
+}
