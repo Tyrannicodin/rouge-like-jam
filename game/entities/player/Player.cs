96598e97bfd0b5c094d20329480477c9cc4b0ba0
Tyrannicodin16 <86689800+Tyrannicodin@users.noreply.github.com>
2023-10-05 22:32:52 +0100

    Prevent crash on invalid crash, add movement line

--- game/entities/player/Player.cs
+++ game/entities/player/Player.cs
@@ -4,6 +4,14 @@ using Godot.Collections;
 
 public class Player : Entity
 {
+    private Line2D pathLine;
+
+    public override void _Ready()
+    {
+        base._Ready();
+
+        pathLine = GetTree().Root.FindNode("Root", true, false).GetNode<Line2D>("PathLine");
+    }
 
     public override void _Input(InputEvent @event)
     {
@@ -22,15 +30,34 @@ public class Player : Entity
                 List<Vector2> path = mapMgr.GetPointPath(currentMapPos, targetPos);
 
                 // @TODO temporary limit to move distance
-                if (MapManager.GetPathDistance(path) > 2)
+                if (MapManager.GetPathDistance(path) > 5 || path.Count == 0)
                 {
                     return;
                 }
 
+                Vector2[] realArray = new Vector2[path.Count];
+                for (int i = 0; i < path.Count; i++)
+                {
+                    realArray[i] = mapMgr.MapToWorld(path[i]);
+                }
+                pathLine.Points = realArray;
                 Move(path, 0.2f);
 
                 EnemyManager.Instance.MoveEnemies(path);
             }
         }
+    }
+
+    protected override void OnMoveFinished()
+    {
+        base.OnMoveFinished();
+        SceneTreeTimer timer = GetTree().CreateTimer(0.2f);
+        timer.Connect("timeout", this, nameof(ClearPoints));
+    }
+
+    private void ClearPoints()
+    {
+        if (moving) return;
+        pathLine.Points = new Vector2[0];
     }
 }
